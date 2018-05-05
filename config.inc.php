b151eb893369f4ee3e3737d06a66ba9496871de1
s402 <s402@domain3.uni-vt.bg>
2018-05-05 11:56:49 +0300

    initial files

---
+++ config.inc.php
@@ -0,0 +1,5 @@
+<?php
+    define("DBSERVER", "localhost");
+    define("DBNAME", "movies_db");
+    define("DBUSER", "root");
+    define("DBPASSWORD", "");
---
+++ movie.php
@@ -0,0 +1,46 @@
+<?php
+require_once('config.inc.php');
+
+class Movie {
+    private $conn;
+
+    function  __construct() {
+        // connect to DB server
+        $this->conn = mysqli_connect(DBSERVER, DBUSER, DBPASSWORD);
+        // select the correct database that we are going to use
+        mysqli_select_db($this->conn, DBNAME) or die(
+            'Error select DB '.DBNAME.' '.mysqli_error($this->conn));
+    }
+
+    function addMovie($title, $directorId) {
+        $query = "INSERT INTO movies(title, directorId) VALUES ('$title','$directorId');";
+
+        mysqli_query($this->conn, $query) or die(
+            'Error insert into DB.<br /> Query: '.$query.'<br /> Error: '.mysqli_error($this->conn). '<br />' );
+
+    }
+    function addDirector($fName, $lName) {
+        $query = "INSERT INTO director(fName, lName) VALUES ('$fName','$lName');";
+
+        mysqli_query($this->conn, $query) or die(
+            'Error insert into DB.<br /> Query: '.$query.'<br /> Error: '.mysqli_error($this->conn). '<br />' );
+
+    }
+
+    function printAllMovies() {
+        $query = "SELECT * FROM movies";
+        // $result holds the information coming from DB
+        $result = mysqli_query($this->conn, $query) or die(
+            'Error select from DB.<br /> Query: '.$query.'<br /> Error: '.mysqli_error($this->conn). '<br />' );
+        // how many results do we have from DB
+        $moviesCount = mysqli_num_rows($result);
+        while ($row = mysqli_fetch_assoc($result)) {
+            print_r($row);
+        }
+        echo "<b>Total number of movies: $moviesCount</b>";
+        mysqli_free_result($result);
+    }
+
+    
+
+}
